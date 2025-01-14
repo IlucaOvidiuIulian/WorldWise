636b1f55349f10a10727bdfc4e77da591fe4d631
Iluca Ovidiu Iulian <ovidiu.ylk@gmail.com>
2025-01-14 20:35:21 +0200

    break from tutorial on 214. : 3.44/11:07

---
+++ src/components/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './map.module.css'
+function map() {
+  return (
+    <div className={styles.mapContainer}>
+      <h1>map</h1>
+    </div>
+  );
+} 
+
+export default map;
